d9fa2d1550e42c9281228ede2cf78e38c63d4af8
soapluke <lukaslindgren@gmail.com>
2019-08-08 21:11:49 +0200

    setting up mongoose

---
+++ services/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ googleID: profile.id }).then((existingUser) => {
+            if (existingUser) {
+
+            } else {
+                new User({ googleID: profile.id }).save();
+            }
+        });
+
+        
+    })
+);
