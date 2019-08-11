38add87635bacd669f1c0b9457111f17ad0e6dcc
soapluke <lukaslindgren@gmail.com>
2019-08-11 14:44:37 +0200

    setup google oauth, mongodb and mongoose

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -13,12 +23,12 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleID: profile.id }).then((existingUser) => {
             if (existingUser) {
-
+                done(null, existingUser);
             } else {
-                new User({ googleID: profile.id }).save();
+                new User({ googleID: profile.id })
+                    .save()
+                    .then(user => done(null, user));
             }
         });
-
-        
     })
 );
